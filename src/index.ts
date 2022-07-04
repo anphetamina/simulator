3388dcd849a2ad6828997456a4961c0e89851fbc
Antonio Santoro <antonio.santoro93@outlook.com>
2022-07-04 11:20:37 +0200

    feat: server+client socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,22 @@
+import http from "http";
+import express from "express";
+import path from "path";
+import morgan from "morgan";
+import { Server } from "socket.io";
+
 import { F1TelemetryClient, constants } from "@racehub-io/f1-telemetry-client";
-import $ from 'jquery'
 const { PACKETS } = constants;
 
 /*
-*   'port' is optional, defaults to 20777
-*   'bigintEnabled' is optional, setting it to false makes the parser skip bigint values,
-*                   defaults to true
-*   'forwardAddresses' is optional, it's an array of Address objects to forward unparsed telemetry to. each address object is comprised of a port and an optional ip address
-*                   defaults to undefined
-*   'skipParsing' is optional, setting it to true will make the client not parse and emit content. You can consume telemetry data using forwardAddresses instead.
-*                   defaults to false
-*/
+ *   'port' is optional, defaults to 20777
+ *   'bigintEnabled' is optional, setting it to false makes the parser skip bigint values,
+ *                   defaults to true
+ *   'forwardAddresses' is optional, it's an array of Address objects to forward unparsed telemetry to. each address object is comprised of a port and an optional ip address
+ *                   defaults to undefined
+ *   'skipParsing' is optional, setting it to true will make the client not parse and emit content. You can consume telemetry data using forwardAddresses instead.
+ *                   defaults to false
+ */
 const client = new F1TelemetryClient({ port: 20777 });
-
-$("#motion").load('public/index.html', () => {
-    client.on(PACKETS.event, console.log);
-    client.on(PACKETS.motion, console.log);
-    client.on(PACKETS.carSetups, console.log);
-    client.on(PACKETS.lapData, console.log);
-    client.on(PACKETS.session, console.log);
-    client.on(PACKETS.participants, console.log);
-    client.on(PACKETS.carTelemetry, console.log);
-    client.on(PACKETS.carStatus, console.log);
-    client.on(PACKETS.finalClassification, console.log);
-    client.on(PACKETS.lobbyInfo, console.log);
-    client.on(PACKETS.carDamage, console.log);
-    client.on(PACKETS.sessionHistory, console.log);
-
-    setTimeout(() => {
-        let el = document.getElementById('motion')
-        if (el) {
-            el.innerHTML = '20'
-        }
-    }, 2000);
-})
-
-
 
 // // to start listening:
 client.start();
@@ -43,4 +24,27 @@ client.start();
 // and when you want to stop:
 // client.stop();
 
-export default client
+const app = express();
+const server = http.createServer(app)
+const io = new Server(server);
+
+app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+Object.keys(PACKETS).forEach((key, idx) => {
+
+  setInterval(() => {
+    client.emit(key, Math.random().toString(36).slice(2, 7))
+  }, 500+1000*idx);
+
+  client.on(key, (data) => {
+    io.emit(key, data)
+  });
+});
+
+server.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+`)
+);
